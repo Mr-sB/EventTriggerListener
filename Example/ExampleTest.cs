3bf3fcfc78ec6b23db301a057d57db5dec7952da
GH-ZJ <wustzhangjie@gmail.com>
2020-03-17 19:09:23 +0800

    add example

---
+++ Example/ExampleTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameUtil.Example
+{
+    public class ExampleTest : MonoBehaviour
+    {
+        public void OnClick(string message)
+        {
+            Debug.Log("on click:" + message);
+        }
+    }
+}
